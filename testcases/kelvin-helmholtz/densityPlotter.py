Fixed first order quadrature point and rotation of frame; negative pressure after some steps though

testcases/kelvin-helmholtz/densityPlotter.py
@@ -8,7 +8,7 @@ import matplotlib.pyplot as plt
 
 #MAX_NUM_INTERACTIONS = 1000
 
-def createPlot(h5File, outDir, plotGrad, iNNL):
+def createPlot(h5File, outDir, plotGrad, plotVel, iNNL):
     data = h5.File(h5File, 'r')
     pos = data["x"][:]
     
@@ -23,14 +23,18 @@ def createPlot(h5File, outDir, plotGrad, iNNL):
     #PPlt = ax.scatter(pos[:,0], pos[:,1], c=P, s=200.) # good for ~400 particles
     
     if "Ghosts" not in str(h5File):
-        #ax.set_xlim((-.75, .75))
-        #ax.set_ylim((-.75, .75))
-        ax.set_xlim((-.6, .6))
-        ax.set_ylim((-.6, .6))
+        ax.set_xlim((-.75, .75))
+        ax.set_ylim((-.75, .75))
+        #ax.set_xlim((-.6, .6))
+        #ax.set_ylim((-.6, .6))
     
     # Plot gradient
-    if plotGrad:
+    if plotGrad and not plotVel:
         plotGradient(data["rhoGrad"][:], pos, ax);
+    elif not plotGrad and plotVel:
+        plotVelocity(data["v"][:], pos, ax)
+    else:
+        print("WARNING: command line arguments '--plotVelocity' and '--plotGradient' are incompatible. - Plotting neither.")
 
     # plot NNL for particle i
     if iNNL > -1 and "Ghosts" not in str(h5File):
@@ -56,11 +60,40 @@ def plotGradient(grad, pos, ax):
     #    if np.linalg.norm(rhoGrad) > .05:
     #        print("rhoGrad @", i, "=", rhoGrad)
 
+def plotVelocity(vel, pos, ax):
+    ax.quiver(pos[:,0], pos[:,1], vel[:,0], vel[:,1], angles='xy', scale_units='xy', scale=10.)
+
+
 def plotNNL(h5File, iNNL, pos, ax):
     data = h5.File(str(h5File).replace(".h5", "NNL.h5"), 'r')
     posNNL = data["nnlPrtcls"+str(iNNL)][:]
     ax.scatter(pos[iNNL,0], pos[iNNL,1], s=50., marker='x', color='b')
     ax.scatter(posNNL[:,0], posNNL[:,1], s=50, marker='x', color='r')
+
+def createEnergyPlot(h5File, outDir):
+    data = h5.File(h5File, 'r')
+    pos = data["x"][:]
+    
+    u = data["u"][()] 
+    fig, ax = plt.subplots(figsize=(8,6), dpi=200)
+    #uPlt = ax.scatter(pos[:,0], pos[:,1], c=u, s=100.) # good for ~900 particles
+    uPlt = ax.scatter(pos[:,0], pos[:,1], c=u, s=200.) # good for ~400 particles
+    #uPlt = ax.scatter(pos[:,0], pos[:,1], c=u, s=10.) # good for 10**4 particles
+    
+    if "Ghosts" not in str(h5File):
+        ax.set_xlim((-.75, .75))
+        ax.set_ylim((-.75, .75))
+        #ax.set_xlim((-.6, .6))
+        #ax.set_ylim((-.6, .6))
+       
+    fig.colorbar(uPlt, ax=ax)
+    plt.title(r"Color coded internal energy $u$")
+    plt.xlabel("$x$")
+    plt.ylabel("$y$")
+    plt.tight_layout()
+    print("Saving figure to", outDir + "/u" + pathlib.Path(h5File).stem + ".png")
+    plt.savefig(outDir + "/u" + pathlib.Path(h5File).stem + ".png")
+    plt.close()
 
 def createPressurePlot(h5File, outDir):
     data = h5.File(h5File, 'r')
@@ -68,13 +101,15 @@ def createPressurePlot(h5File, outDir):
     
     P = data["P"][()] 
     fig, ax = plt.subplots(figsize=(8,6), dpi=200)
-    PPlt = ax.scatter(pos[:,0], pos[:,1], c=P, s=100.) # good for ~900 particles
+    PPlt = ax.scatter(pos[:,0], pos[:,1], c=P, s=200.) # good for ~400 particles
+    #PPlt = ax.scatter(pos[:,0], pos[:,1], c=P, s=100.) # good for ~900 particles
+    #PPlt = ax.scatter(pos[:,0], pos[:,1], c=P, s=10.) # good for 10**4 particles
     
     if "Ghosts" not in str(h5File):
-        #ax.set_xlim((-.75, .75))
-        #ax.set_ylim((-.75, .75))
-        ax.set_xlim((-.6, .6))
-        ax.set_ylim((-.6, .6))
+        ax.set_xlim((-.75, .75))
+        ax.set_ylim((-.75, .75))
+        #ax.set_xlim((-.6, .6))
+        #ax.set_ylim((-.6, .6))
        
     fig.colorbar(PPlt, ax=ax)
     plt.title(r"Color coded pressure $P$")
@@ -84,7 +119,7 @@ def createPressurePlot(h5File, outDir):
     print("Saving figure to", outDir + "/P" + pathlib.Path(h5File).stem + ".png")
     plt.savefig(outDir + "/P" + pathlib.Path(h5File).stem + ".png")
     plt.close()
-
+    
    
 if __name__=="__main__":
     parser = argparse.ArgumentParser(description="Plot density of results from Kelvin-Helmholtz test case.")
@@ -93,6 +128,8 @@ if __name__=="__main__":
     parser.add_argument("--plotGradient", "-g", action="store_true", help="plot density gradients")
     parser.add_argument("--plotGhosts", "-G", action="store_true", help="also plot ghost cells in an extra file")
     parser.add_argument("--pressure", "-P", action="store_true", help="plot pressure instead of density")
+    parser.add_argument("--energy", "-u", action="store_true", help="plot internal energy instead of density")
+    parser.add_argument("--plotVelocity", "-v", action="store_true", help="plot velocity")
     parser.add_argument("--iNNL", "-i", metavar="int", type=int, help="plot NNL for particles i", default=-1)
 
     args = parser.parse_args()
@@ -107,10 +144,14 @@ if __name__=="__main__":
                 print("\t", h5File)
                 if args.pressure:
                     if args.plotGradient or args.iNNL > -1:
-                        print("WARNING: command line arguments --plotGradient and --iNNL are ignored when plotting pressure.")
+                        print("WARNING: command line arguments '--plotGradient' and '--iNNL' are ignored when plotting pressure.")
                     createPressurePlot(h5File, args.outDir)
+                elif args.energy:
+                    if args.plotGradient or args.iNNL > -1:
+                        print("WARNING: command line arguments '--plotGradient' and '--iNNL' are ignored when plotting energy.")
+                    createEnergyPlot(h5File, args.outDir)
                 else:
-                    createPlot(h5File, args.outDir, args.plotGradient, args.iNNL)
+                    createPlot(h5File, args.outDir, args.plotGradient, args.plotVelocity, args.iNNL)
             
 
     print("... done.")

testcases/kelvin-helmholtz/generateIC.py
@@ -25,7 +25,7 @@ def getVelsX(y):
     mask3 = (.5<=y) & (y<=.75)
     velsX[mask3] = .5-.5*np.exp((y[mask3]-.75)/Dy)
     mask4 = .75<y
-    velsX[mask4] = -.5-.5*np.exp((.75-y[mask4])/Dy)
+    velsX[mask4] = -.5+.5*np.exp((.75-y[mask4])/Dy)
 
     return velsX
 